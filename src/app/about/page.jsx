20ab5e72f9bd888f9dc9b2456239e1892ffb9476
hasanarpat <black_the_dragon@hotmail.com>
2023-08-30 17:49:52 +0300

    next-learning

---
+++ src/app/about/page.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/Button";
+
+export const metadata = {
+  title: "About Crowgenstein",
+  description: "A multifunctional web app for showing skills.",
+};
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/2977547/pexels-photo-2977547.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          fill={true}
+          alt=""
+          className={styles.img}
+        />
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h2 className={styles.imgDesc}>
+            handcrafting award winning digital experiences
+          </h2>
+        </div>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who Are we?</h1>
+          <p className={styles.desc}>
+            Aliqua nostrud laborum id amet ipsum non aliquip occaecat
+            exercitation pariatur reprehenderit tempor tempor. Mollit aliqua eu
+            voluptate nisi ut aliquip nulla officia excepteur aute dolor Lorem.
+            Est dolor sint cupidatat magna elit eiusmod nulla.
+            <br />
+            <br />
+            Enim Lorem consectetur consectetur ullamco est commodo ad aliquip
+            cupidatat velit dolore labore. In ipsum irure ex tempor incididunt
+            ad id ut excepteur magna. Sint incididunt ea et eiusmod quis quis.
+            Sint et in proident nisi dolore ullamco dolor ut non adipisicing.
+          </p>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who Are we?</h1>
+          <p className={styles.desc}>
+            Ad sint nostrud eiusmod commodo veniam deserunt voluptate non dolor
+            irure. Voluptate dolore occaecat excepteur Lorem commodo duis enim
+            ullamco adipisicing culpa mollit ad. Reprehenderit nisi fugiat
+            incididunt et magna. Et id cillum adipisicing cillum dolore dolor
+            ipsum adipisicing mollit. 
+            <br/><br/>
+            - Dynamic Websites <br/><br/>- Fast and Handy 
+            <br/><br/>- Mobile Apps
+          </p>
+          <Button url="/contact" text="Contact"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -0,0 +1,52 @@
+"use client"
+import React, { useState } from 'react'
+import styles from "./page.module.css"
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
+const Register = () => {
+  const [err,setErr] = useState(false)
+  const router = useRouter()
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    console.log(name,email,password)
+
+    try {
+      const res = await fetch("/api/auth/register",{
+        method:"POST",
+        headers:{
+          "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({name,email,password})
+
+      })
+      res.status===201 & router.push("/dashboard/login?success=Account has been created")
+    } catch (error) {
+      console.log(error)
+      setErr(true)
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input placeholder='username' type='text' className={styles.input} required/>
+        <input placeholder='email' type='email' className={styles.input} required/>
+        <input placeholder='pasword' type='password' className={styles.input} required/>
+        <button className={styles.button}>Register</button>
+      </form>
+      {
+        err && "Something went wrong."
+      }
+    <Link href="/dashboard/login">Log in with an existing account.</Link>
+    </div>
+  )
+}
+
+export default Register
